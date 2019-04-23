d6c3d368086f36f5e55f871ed5a50cf1a189518a
mabat25 <mabat250@gmail.com>
2019-04-23 14:25:45 +0200

    added resource blueprint

---
+++ StarGame/Resource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarGame
+{
+    class Resource
+    {
+        public const int BarScale = 10;
+        public int fuel = 100;
+        public int oxygen = 100;
+        public int hull = 100;
+        public int electric = 100;
+        public Dictionary<string, int> other = new Dictionary<string, int>();
+        public Resource()
+        {
+
+        }
+        private int ToBar(int value)
+        {
+            return value / BarScale;
+        }
+    }
+}
