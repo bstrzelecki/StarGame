77c24cfa46b088d3f99fa8aac9a936cf074eb018
mabat25 <mabat250@gmail.com>
2019-05-10 17:00:33 +0200

    added simple station ai

---
+++ StarGame/EnemyStation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarGame
+{
+    class EnemyStation : Planet
+    {
+        public bool HasJammer { get; set; }
+        public int hp= 100;
+        public Weapon weapon;
+        public EnemyStation(Sprite sprite, float mass, float distance) : base(sprite, mass, distance)
+        {
+
+        }
+        
+        public void Fire()
+        {
+            Vector2 position;
+            position = Physics.GetForwardVector(Period) * distance + (center is StarSystem str ? str.position : (center is Planet pl ? ((StarSystem)pl.center).position : Vector2.Zero)) + Input.cameraOffset;
+            if(weapon != null)
+                weapon.SpawnProjectile(position, Input.GetDegree((float)Math.Sin((MainScene.player.position - position).Length())),Vector2.Zero);
+        }
+        
+    }
+}
--- StarGame/Player/Weapon.cs
+++ StarGame/Player/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,28 @@ namespace StarGame
                 if (Cooldown < 1)
                     Cooldown += CooldownTime;
             };
+        }
+        public virtual void SpawnProjectile(Vector2 position, float rotation, Vector2 velocity)
+        {
+            if (Cooldown < 1 || MainScene.barArray.GetResource("power") <= 0)
+            {
+                return;
+            }
+            Projectile p = (Projectile)projectile.Clone();
+            p.Position = position + Physics.GetForwardVector(rotation) * 30;
+            p.physics.velocity = velocity + Physics.GetForwardVector(rotation) * 100;
+            Time.OnTick += () =>
+            {
+                if (!p.IsDisposed)
+                {
+                    p.Position += p.physics.velocity;
+                    p.Decay -= 0.1f;
+                }
+            };
+            p.Rotation = Input.GetRads(rotation) + (float)Math.PI / 2;
+            Projectiles.Add(p);
+            Cooldown = 0;
+            MainScene.barArray.SubtractResource("power", 5);
         }
         public virtual void SpawnProjectile()
         {
