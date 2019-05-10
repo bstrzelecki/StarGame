e3df4bfd8bf13fdeb11d3247ee16cc80026875c6
mabat25 <mabat250@gmail.com>
2019-05-10 17:11:46 +0200

    minor changes

--- StarGame/EnemyStation.cs
+++ StarGame/EnemyStation.cs
@@ -7,23 +7,37 @@ using System.Threading.Tasks;
 
 namespace StarGame
 {
-    class EnemyStation : Planet
+    class EnemyStation : Planet, IUpdateable
     {
         public bool HasJammer { get; set; }
         public int hp= 100;
         public Weapon weapon;
         public EnemyStation(Sprite sprite, float mass, float distance) : base(sprite, mass, distance)
         {
-
+            Game1.RegisterUpdate(this);
         }
-        
+
+        private Vector2 _position = Vector2.Zero;
+        public Vector2 Position {
+            get
+            {
+                _position = Physics.GetForwardVector(Period) * distance + (center is StarSystem str ? str.position : (center is Planet pl ? ((StarSystem)pl.center).position : Vector2.Zero)) + Input.cameraOffset;
+                return _position;
+            }
+        }
+
         public void Fire()
         {
-            Vector2 position;
-            position = Physics.GetForwardVector(Period) * distance + (center is StarSystem str ? str.position : (center is Planet pl ? ((StarSystem)pl.center).position : Vector2.Zero)) + Input.cameraOffset;
             if(weapon != null)
-                weapon.SpawnProjectile(position, Input.GetDegree((float)Math.Sin((MainScene.player.position - position).Length())),Vector2.Zero);
+                weapon.SpawnProjectile(Position, Input.GetDegree((float)Math.Sin((MainScene.player.position - Position).Length())),Vector2.Zero);
         }
-        
+
+        public void Update()
+        {
+            if(Vector2.Distance(Position, MainScene.player.position) < 2000)
+            {
+                Fire();
+            }
+        }
     }
 }
