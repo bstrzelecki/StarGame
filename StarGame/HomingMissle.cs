5d85d6a1985c512f5113fd7559abbf6721b51fed
mabat25 <mabat250@gmail.com>
2019-06-08 17:43:58 +0200

    Debuging

--- StarGame/HomingMissle.cs
+++ StarGame/HomingMissle.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace StarGame
 {
-    internal class HomingMissle : Projectile
+    internal class HomingMissle : Projectile, IDisposable
     {
         public ITargetable target;
         public HomingMissle(ITargetable target, Sprite sprite) : base(sprite)
@@ -14,8 +15,16 @@ namespace StarGame
         }
 
         private Vector2 initialVelocity = Vector2.Zero;
+        private int decay = 500;
         private void Time_OnTick()
         {
+            if (IsDisposed) return;
+            if (decay == 0)
+            {
+                Dispose();
+                return;
+            }
+            decay--;
             if (initialVelocity == Vector2.Zero)
             {
                 initialVelocity = physics.velocity;
@@ -40,11 +49,6 @@ namespace StarGame
         }
         public override void Draw(SpriteBatch sprite)
         {
-            if (Decay < 0)
-            {
-                Dispose();
-            }
-
             if (!IsDisposed)
             {
                 sprite.Draw(Sprite, Position + Input.cameraOffset, null, Color.White, Rotation - Input.GetRads(90), new Vector2(Sprite.Size.Width / 2, Sprite.Size.Height / 2), 1, SpriteEffects.None, 0);
